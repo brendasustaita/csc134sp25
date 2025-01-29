c778af686d2513644ebd2ab2b2413d985eb92ced
brendasustaita <157392809+brendasustaita@users.noreply.github.com>
2025-01-29 22:29:54 +0000

    completed hw

---
+++ M1/M1HW_Sustaita.cpp
@@ -0,0 +1,24 @@
+// CSC 134
+// M1HW
+// Brenda Sustaita
+// 01/29/2024
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    string name = "Harry Potter and the Goblet of Fire"; //fav movie
+    int year = 2005; // year movie was released
+    double box_office = 897.5;
+    // form statement
+    cout << "The movie " << name << " was made in " << year << "." << endl;
+    cout << "\"Harry Potter and the Goblet of Fire\" made $" << box_office << " million USD. This is the 7th highest-ranking movie in the Harry Potter series." << endl;
+    cout << "This movie is the fourth movie out of the collection." << endl;
+    cout << "The movie has reoccurring movie characters that play the main roles like Daniel Radcliffe, Emma Watson, and Rupert Grint." << endl;
+    cout << endl;
+    cout << "This movie was known for the scene where Cedric Diggory, played by Robert Patterson, was killed by Voldemort in the Goblet of Fire tournament." << endl;
+    cout << endl;
+
+
+    return 0;
+}
