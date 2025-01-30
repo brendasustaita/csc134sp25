9619a6bd989dfde2be659255cb7857d48507b471
brendasustaita <157392809+brendasustaita@users.noreply.github.com>
2025-01-30 00:07:32 +0000

    completed lab

---
+++ M1/M1LAB1_Sustaita.cpp
@@ -0,0 +1,27 @@
+// CSC 134
+// Brenda Sustaita
+// M1LAB1 - Apple Sales 
+// 01/29/2025
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() 
+{
+    string name = "Brenda"; //my name
+    int apples = 100; //int are whole numbers, number of apples we have
+    double cost_each = 0.25; //twenty five cents, price per apple
+    double totalprice = (double) apples * cost_each;
+    //print all info
+    cout << "Welcome to " << name;
+    cout << "'s apple orchard!" << endl;
+    cout << "We have " << apples;
+    cout << " apples in stock." <<endl;
+    cout << "Apples are currently $";
+    cout << cost_each << " each." << endl;
+    //find out total
+    cout << "If you would like them all, it will be $" << totalprice <<"." << endl;
+    cout << endl;
+}
+
