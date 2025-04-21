3b4b80a8b46bfed70327be22c038c6e66be9e588
brendasustaita <157392809+brendasustaita@users.noreply.github.com>
2025-04-20 22:19:03 -0400

    Update 3MT3_Sustaita.cpp

--- M3/3MT3_Sustaita.cpp
+++ M3/3MT3_Sustaita.cpp
@@ -4,8 +4,42 @@
 // 02/24/2025
 
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
-int main (){
+int main ()
+{
+    cout << "Let's roll some dice!" << endl;
+    int seed = time(0)
+    cout << "Your seed is: " << seed << endl;
+    srand(seed);
+
+    for (int i = 1; i <= 10l; i++)
+        {
+            int d1 = rand() % 6 + 1;
+            int d2 = rand() % 6 + 1:
+            int sum = d1 + d2;
+            
+            cout<< "Roll" << i << ":" <<  d1 << " + " << d2 << " = " << sum << " -> ";
+
+            if (sum == 7 || sum == 11)
+            {
+                cout << "WIN!" << endl; 
+            }
+
+            else if (sum == 2 || sum == 3 || sum == 12)
+            {
+                cout << "CRAP!" << endl;
+            }
+
+            else
+            {
+                cout << "Point established at " << sum << endl;
+            }
+        }
+    cout << endl;
     return 0;
 }
